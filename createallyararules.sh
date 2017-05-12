4a154b80429482bd5e4989a522c59ae08d45c66f
benrau87 <benrau87@users.noreply.github.com>
2017-05-12 09:51:09 -0500

    Update createallyararules.sh

--- createallyararules.sh
+++ createallyararules.sh
@@ -1,20 +1,29 @@
 #!/bin /bash
 #use to create an index to include all yara rules in the /etc/VolUtility/yararules/   path
 cd /etc/VolUtility/
-
+#check for exisiting rule file and remove
 while [ ! -f /etc/VolUtility/yararules/allrules.yar ]
 do
   rm /etc/VolUtility/yararules/allrules.yar
 done
-
-git clone https://github.com/VirusTotal/yara.git
-rm etc/VolUtility/rules/Android* 
-rm etc/VolUtility/rules/vmdetect.yar  
-rm etc/VolUtility/rules/antidebug_antivm.yar  
-rm etc/VolUtility/rules/MALW_AdGholas.yar  
-rm etc/VolUtility/rules/APT_Shamoon*.yar  
-rm etc/VolUtility/rules/peid.yar 
-cp rules/**/*.yar /etc/VolUtility/yararules/ &>> $logfile
+#check for existing rule directory and update
+if [-d /etc/VolUtility/rules ]; then
+    cd /etc/VolUtility/rules
+    git pull
+else
+  git clone https://github.com/VirusTotal/yara.git
+fi
+#Remove non working rules
+cd /etc/VolUtility/
+rm rules/Android* 
+rm rules/vmdetect.yar  
+rm rules/antidebug_antivm.yar  
+rm rules/MALW_AdGholas.yar  
+rm rules/APT_Shamoon*.yar  
+rm rules/peid.yar 
+#Copy over rule 
+cp rules/**/*.yar /etc/VolUtility/yararules/
+#Create allrule.yar index
 touch allrules.yar
 ls /etc/VolUtility/yararules >allrules.yar
 sed -i -e 's/^/include "/' allrules.yar
