b8ec5f8761facc2d6c8e6427da915db38b798dc8
benrau87 <benrau87@users.noreply.github.com>
2017-05-12 09:34:29 -0500

    Update createallyararules.sh

--- createallyararules.sh
+++ createallyararules.sh
@@ -1,6 +1,8 @@
 #!/bin /bash
 #use to create an index to include all yara rules in the /etc/VolUtility/yararules/   path
+cd /etc/VolUtility/
 rm /etc/VolUtility/yararules/allrules.yar
+git clone https://github.com/VirusTotal/yara.git
 cp rules/**/*.yar /etc/VolUtility/yararules/ &>> $logfile
 touch allrules.yar
 ls /etc/VolUtility/yararules >allrules.yar
